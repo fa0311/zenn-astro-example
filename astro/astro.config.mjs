041228e56e2f841db795d8dc7686c0dac9e3705f
ふぁ <yuki@yuki0311.com>
2025-01-21 14:17:11 +0900

    add google analytics

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,8 +1,16 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import partytown from "@astrojs/partytown";
+
 export default defineConfig({
-  integrations: [],
+  integrations: [
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
   output: "static",
   trailingSlash: "never",
   site: "https://fa0311.github.io",
